b604d750abca8994cc3c7e06915f627893f40aa3
Damien JANDARD <damien.jandard@gmail.com>
2023-08-07 11:48:54 +0200

    feat: Setting up customer creation

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Repository\CustomerRepository;
 use JMS\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,6 +29,19 @@ class CustomerController extends AbstractController
     public function showCustomer(Customer $customer, SerializerInterface $serializer): JsonResponse
     {
         $this->denyAccessUnlessGranted('CUSTOMER_BELONGS_TO_ME', $customer, 'Access denied, you do not have the necessary permissions to view this record.');
+        $context = SerializationContext::create()->setGroups('getCustomers');
+        $jsonCustomer = $serializer->serialize($customer, 'json', $context);
+        return new JsonResponse($jsonCustomer, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/customer', name: 'add', methods: [Request::METHOD_POST])]
+    public function addCustomer(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    {
+        $customer = $serializer->deserialize($request->getContent(), Customer::class, 'json');
+        $customer->setOwner($this->getUser())
+            ->setCreatedAt(new \DateTimeImmutable());
+        $em->persist($customer);
+        $em->flush();
         $context = SerializationContext::create()->setGroups('getCustomers');
         $jsonCustomer = $serializer->serialize($customer, 'json', $context);
         return new JsonResponse($jsonCustomer, Response::HTTP_OK, [], true);
