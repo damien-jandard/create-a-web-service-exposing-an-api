cd9a90f2bb6af40f967882aa614a18068647d54c
Damien JANDARD <damien.jandard@gmail.com>
2023-08-09 10:38:44 +0200

    feat: Setting up pagination for the list of products and the list of customers

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,26 +5,39 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use JMS\Serializer\SerializationContext;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('api', name: 'app_product_')]
 class ProductController extends AbstractController
 {
-    #[Route('api/products', name: 'app_product_list', methods: [Request::METHOD_GET])]
-    public function list(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    #[Route('/products', name: 'list', methods: [Request::METHOD_GET])]
+    public function list(ProductRepository $productRepository, SerializerInterface $serializer, Request $request, PaginatorInterface $paginator): JsonResponse
     {
-        $productList = $productRepository->findAll();
-        $jsonProductList = $serializer->serialize($productList, 'json');
+        $productList = $paginator->paginate(
+            $productRepository->findAllWithPagination(), 
+            (int) $request->get('page', 1),
+            (int) $request->get('limit', 5)
+        );
+        if ((int) $request->get('page', 1) > ceil($productList->getTotalItemCount() / 5)) {
+            throw new HttpException(JsonResponse::HTTP_NOT_FOUND, 'The requested page does not exist.');
+        }
+        $context = SerializationContext::create()->setGroups('getProducts');
+        $jsonProductList = $serializer->serialize($productList->getItems(), 'json', $context);
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
 
-    #[Route('api/product/{id}', name: 'app_product_show', methods: [Request::METHOD_GET])]
+    #[Route('/product/{id}', name: 'show', methods: [Request::METHOD_GET])]
     public function show(Product $product, SerializerInterface $serializer): JsonResponse
     {
-        $jsonProduct = $serializer->serialize($product, 'json');
+        $context = SerializationContext::create()->setGroups('getProducts');
+        $jsonProduct = $serializer->serialize($product, 'json', $context);
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
 }
--- src/Repository/CustomerRepository.php
+++ src/Repository/CustomerRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Customer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<Customer>
@@ -16,33 +18,20 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CustomerRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 5;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Customer::class);
     }
 
-//    /**
-//     * @return Customer[] Returns an array of Customer objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Customer
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findAllWithPagination(UserInterface $user): Query
+    {
+        $query = $this->createQueryBuilder('c')
+            ->where('c.owner = :user')
+            ->setParameter('user', $user)
+            ->orderBy('c.id', 'ASC')
+        ;
+        return $query->getQuery();
+    }
 }
